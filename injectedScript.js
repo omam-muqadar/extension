f28247573e9a3097a483514425d1d40f161b621b
Omam Muqadar <omam.muqadar@lethub.co>
2025-01-23 21:26:12 +0500

    filter popup

--- injectedScript.js
+++ injectedScript.js
@@ -6,7 +6,7 @@ const RETRY_INTERVAL = 1000 // Interval in ms between retries
   let retryCount = 0
 
   function retrieveUserData() {
-    console.log("Attempting to retrieve user data from localStorage...")
+    // console.log("Attempting to retrieve user data from localStorage...")
 
     const userId = localStorage.getItem("userId")
     const userEmail = localStorage.getItem("userEmail")
@@ -15,11 +15,11 @@ const RETRY_INTERVAL = 1000 // Interval in ms between retries
     const role = localStorage.getItem("role")
     const token = localStorage.getItem("token")
 
-    console.log("userId:", userId)
-    console.log("userEmail:", userEmail)
-    console.log("username:", username)
-    console.log("companyName:", companyName)
-    console.log("role:", role)
+    // console.log("userId:", userId)
+    // console.log("userEmail:", userEmail)
+    // console.log("username:", username)
+    // console.log("companyName:", companyName)
+    // console.log("role:", role)
 
     if (userId && userEmail && username && companyName && role && token) {
       const userData = {
@@ -31,13 +31,13 @@ const RETRY_INTERVAL = 1000 // Interval in ms between retries
         token: token
       }
 
-      console.log("All required data found. Posting message:", userData)
+      // console.log("All required data found. Posting message:", userData)
       window.postMessage(
         { type: "USER_ID_FROM_LOCALSTORAGE", userData: userData },
         "*"
       )
     } else {
-      console.log("Required data missing. Retrying in 500ms...")
+      // console.log("Required data missing. Retrying in 500ms...")
       setTimeout(retrieveUserData, 500) // 500ms delay
     }
   }
